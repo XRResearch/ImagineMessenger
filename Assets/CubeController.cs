30bf65dd22605b84ec3f50adf80bac8fb0c8692a
quentinPitalier <quentin.pitalier@gmail.com>
2017-10-17 00:15:29 +0300

    Assets, VR (Daydream) and First Draft world
    This first commit focus on assets and VR ressources usefull for the
    project. It contains also a first draft of a "forest world", including
    some "Lights orbs" offering the possibility to move in the world to the
    user.

---
+++ Assets/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void fallingToGround(){
+        this.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed;
+
+    public Vector3 nextPosition;
+    public bool isMoving;
+
+
+    // Use this for initialization
+    void Start () {
+
+        isMoving = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+            if(this.transform.position == nextPosition)
+            {
+                isMoving = false;
+            }
+        }
+
+	}
+
+    public void moveToPoint(GameObject point) {
+
+        if (!isMoving) {
+            nextPosition = point.transform.position;
+            isMoving = true;
+        }
+        
+    }
+}
